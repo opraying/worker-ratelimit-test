c737b431152dbc4d036ced1a5126f051beb02f29
Ray <uek0819@gmail.com>
2024-10-10 12:15:52 +0800

    wip

--- src/handle.ts
+++ src/handle.ts
@@ -1,9 +1,8 @@
+import { HttpServerRequest } from "@effect/platform"
 import * as HttpApiBuilder from "@effect/platform/HttpApiBuilder"
 import * as DateTime from "effect/DateTime"
 import * as Effect from "effect/Effect"
-import * as Redacted from "effect/Redacted"
 import { MyHttpApi } from "./api"
-import { AccessTokenString, Account, AccountId, UserId, UserWithSensitive } from "./model"
 
 export const HttpAppLive = HttpApiBuilder.group(MyHttpApi, "app", (handles) =>
   Effect.gen(function*() {
@@ -12,21 +11,18 @@ export const HttpAppLive = HttpApiBuilder.group(MyHttpApi, "app", (handles) =>
     return handles.pipe(
       HttpApiBuilder.handle("index", () =>
         Effect.gen(function*() {
-          const createdAt = yield* DateTime.now
-          const updatedAt = yield* DateTime.now
+          const now = yield* DateTime.now
+          const serverRequest = yield* HttpServerRequest.HttpServerRequest
+          const headers = serverRequest.headers
+          const ip = headers["cf-connecting-ip"] || headers["x-forwarded-for"] || headers["x-real-ip"] || "unknown"
 
-          return UserWithSensitive.make({
-            id: UserId.make(1),
-            accessToken: Redacted.make(AccessTokenString.make("123")),
-            account: Account.make({
-              id: AccountId.make(1),
-              createdAt,
-              updatedAt
-            }),
-            accountId: AccountId.make(1),
-            createdAt,
-            updatedAt
-          })
+          const ratelimitKey = ip
+
+          const { success } = yield* Effect.promise(() => globalThis.env.RATE_LIMITER.limit({ key: ratelimitKey }))
+
+          const message = success ? `IP: ${ip} - Hello - ${now}` : `IP: ${ip} - Too many requests`
+
+          return message
         })),
       HttpApiBuilder.handle("health", () => Effect.succeed("ok"))
     )
